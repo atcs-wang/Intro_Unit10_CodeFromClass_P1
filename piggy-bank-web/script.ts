fa2b0d7480105dec473201e074c16ed7edca778e
Matthew Wang <matwan@bergen.org>
2022-04-05 08:38:11 -0400

    piggy bank - refactor for data- attributes, nested functions,  querySelectorAll

--- piggy-bank-web/script.ts
+++ piggy-bank-web/script.ts
@@ -1,11 +1,6 @@
 const balanceElm = document.querySelector(".balance") as HTMLElement;
 const balanceAmountElm = document.querySelector("#balance-amount") as HTMLElement;
 
-const pennyElm = document.querySelector("#penny") as HTMLElement;
-const nickelElm = document.querySelector("#nickel") as HTMLElement;
-const dimeElm = document.querySelector("#dime") as HTMLElement;
-const quarterElm = document.querySelector("#quarter") as HTMLElement;
-
 const coinAddSoundElm = document.querySelector("audio.coin-add") as HTMLAudioElement;
 const coinRemoveSoundElm = document.querySelector("audio.coin-remove") as HTMLAudioElement;
 
@@ -24,89 +19,46 @@ function updateBalance(amount : number, remove : boolean) : void {
 function handleCoinClick(event : MouseEvent) {
     let clickedElm = event.currentTarget as HTMLElement;
     updateBalance( Number(clickedElm.dataset.amount), event.shiftKey);
-    addToHistory(clickedElm.dataset.letter as string, event.shiftKey);
+    addToHistory(clickedElm, event.shiftKey);
 }
-pennyElm.onclick = handleCoinClick
-nickelElm.addEventListener("click", handleCoinClick); 
-dimeElm.onclick = handleCoinClick
-quarterElm.addEventListener("click", handleCoinClick);
+
+document.querySelectorAll<HTMLElement>(".coin").forEach(function(element : HTMLElement) {
+    element.addEventListener("click", handleCoinClick); 
+})
 
 document.addEventListener("keydown", (event: KeyboardEvent) => {
     if (event.repeat)
         return;
-    let coinValue : number = 0;
-    switch (event.key) {
-        case "P":case "p":
-            coinValue = 0.01;
-            pennyElm.classList.add("keypress");
-        break;
-        case "N":case "n":
-            coinValue = 0.05;
-            nickelElm.classList.add("keypress");
-        break;
-        case "D:":case "d":
-            coinValue = 0.10;
-            dimeElm.classList.add("keypress");
-        break;
-        case "Q":case "q":
-            coinValue = 0.25;
-            quarterElm.classList.add("keypress");
-        break;
-        default:
-            return; //stop if not one of  these keys
+    //get some .coin element with data-letter matching event.key
+    let elm = document.querySelector(`.coin[data-letter='${event.key.toLowerCase()}']`) as HTMLElement | null
+    if (!elm){ //elm === null
+        return;
     }
-    
+    elm.classList.add("keypress");
     let doIRemove : boolean = event.shiftKey;
-    updateBalance(coinValue, doIRemove);
-    addToHistory(event.key.toLowerCase(), doIRemove);
+
+    updateBalance(Number(elm.dataset.amount), doIRemove);
+    addToHistory(elm, doIRemove);
 });
 
 document.addEventListener("keyup", (event: KeyboardEvent) => {
-    switch (event.key) {
-        case "P":case "p":
-            pennyElm.classList.remove("keypress");
-        break;
-        case "N":case "n":
-            nickelElm.classList.remove("keypress");
-        break;
-        case "D:":case "d":
-            dimeElm.classList.remove("keypress");
-        break;
-        case "Q":case "q":
-            quarterElm.classList.remove("keypress");
-        break;
-        default:
-            return; //stop if not one of  these keys
+    //get some .coin element with data-letter matching event.key
+    let elm = document.querySelector(`.coin[data-letter='${event.key.toLowerCase()}']`) as HTMLElement | null
+    if (!elm){ //elm === null
+        return;
     }
+    elm.classList.remove("keypress");
 })
 
-function addToHistory(coin : string, negative : boolean) {
+function addToHistory(coinElement : HTMLElement, negative : boolean) {
     let liElm : HTMLLIElement = document.createElement("li");
-    liElm.textContent = coin;
+    liElm.textContent = coinElement.dataset.letter as string;
     if (negative) 
         liElm.classList.add("negative");
     historyElm.appendChild(liElm);
 
-    liElm.addEventListener("click", removeFromHistory);
-}
-
-function removeFromHistory(event : MouseEvent) {
-    let elm = event.currentTarget as HTMLElement;
-    elm.remove();
-    //undo the original balance change
-    switch(elm.textContent) {
-        case "p":
-            updateBalance(0.01, ! elm.classList.contains("negative"));
-        break;
-        case "n":
-            updateBalance(0.05, ! elm.classList.contains("negative"));
-        break;
-        case "d":
-            updateBalance(0.10, ! elm.classList.contains("negative"));
-        break;
-        case "q":
-            updateBalance(0.25, ! elm.classList.contains("negative"));
-        break;
-        
-    }
+    liElm.addEventListener("click", function(event : MouseEvent){
+        liElm.remove();
+        updateBalance(Number(coinElement.dataset.amount), ! negative);
+    });
 }
